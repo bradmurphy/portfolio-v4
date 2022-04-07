01df8d95185e73b7f16270961ff4583f00d2997a
Brad Murphy <bm.dev84@gmail.com>
2022-04-06 23:57:34 -0400

    Updating viewport

--- pages/_app.js
+++ pages/_app.js
@@ -1,14 +1,11 @@
-import "../styles/index.sass";
-import Head from "next/head";
+import '../styles/index.sass';
+import Head from 'next/head';
 
 function MyApp({ Component, pageProps }) {
   return (
     <>
       <Head>
-        <meta
-          name="viewport"
-          content="width=device-width, initial-scale=1.0, maximum-scale=1.0"
-        />
+        <meta name="viewport" content="width=device-width, initial-scale=1.0" />
       </Head>
       <Component {...pageProps} />
     </>
